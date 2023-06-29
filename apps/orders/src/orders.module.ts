31751602347470db1942e742f1dd973efddb94f2
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-06-29 16:11:26 -0300

    :tada: initial commit

---
+++ apps/orders/src/orders.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { OrdersController } from './orders.controller';
+import * as Joi from 'joi';
+import { OrdersService } from './orders.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '@app/common';
+import { OrdersRepository } from './repositories/orders-repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/orde-schema';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+    DatabaseModule,
+    MongooseModule.forFeature([
+      {
+        name: Order.name,
+        schema: OrderSchema,
+      },
+    ]),
+  ],
+  controllers: [OrdersController],
+  providers: [OrdersService, OrdersRepository],
+})
+export class OrdersModule {}
