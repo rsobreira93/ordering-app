3441eddf58f2e6b537b5534c5fb7868b1a91a7ab
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-07-08 14:25:15 -0300

    feat: add new route

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { OrdersService } from './orders.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateOrdersService, GetOrdersService } from './services';
 import { CreateOrderDTO } from './dtos/create-order';
 
 @Controller('orders')
 export class OrdersController {
-  constructor(private readonly ordersService: OrdersService) {}
-
+  constructor(
+    private readonly createOrdersService: CreateOrdersService,
+    private readonly getOrdersService: GetOrdersService,
+  ) {}
   @Post()
   async createOrder(@Body() request: CreateOrderDTO) {
-    return await this.ordersService.execute(request);
+    return await this.createOrdersService.execute(request);
+  }
+
+  @Get()
+  async getOrders() {
+    return await this.getOrdersService.execute();
   }
 }
