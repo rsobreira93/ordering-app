6fb6cb77aa6fedc5433fb7b3ecff94ad91f4504f
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-07-08 14:24:06 -0300

    refactor: adjust name file and class

--- apps/orders/src/services/create-orders.service.ts
+++ apps/orders/src/services/create-orders.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { CreateOrderDTO } from './dtos/create-order';
-import { OrdersRepository } from './repositories/orders-repository';
+import { CreateOrderDTO } from '../dtos/create-order';
+import { OrdersRepository } from '../repositories/orders-repository';
 
 @Injectable()
-export class OrdersService {
+export class CreateOrdersService {
   constructor(private readonly ordersRepository: OrdersRepository) {}
   async execute(data: CreateOrderDTO) {
     return await this.ordersRepository.create(data);
