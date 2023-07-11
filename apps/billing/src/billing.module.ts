b60fd7ba78358f27a1a58eaf6e59b86b70b6522b
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-07-11 13:51:32 -0300

    feat: add env path file to confgMoudule

--- apps/billing/src/billing.module.ts
+++ apps/billing/src/billing.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BillingController } from './billing.controller';
-import { BillingService } from './billing.service';
+import { BillingService } from './services/billing.service';
 import { RmqModule } from '@app/common';
 import { ConfigModule } from '@nestjs/config';
-import Joi from 'joi';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
@@ -13,6 +13,7 @@ import Joi from 'joi';
         RABBIT_MQ_URI: Joi.string().required(),
         RABBIT_MQ_BILLING_QUEUE: Joi.string().required(),
       }),
+      envFilePath: './apps/billing/.env',
     }),
     RmqModule,
   ],
