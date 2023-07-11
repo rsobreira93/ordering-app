b24373540c1fee2bdb32654545a01c5397477937
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-07-11 13:52:24 -0300

    feat: add const to rabbitMQ module

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -3,11 +3,12 @@ import { OrdersController } from './orders.controller';
 import * as Joi from 'joi';
 import { CreateOrdersService } from './services/create-orders.service';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, RmqModule } from '@app/common';
 import { OrdersRepository } from './repositories/orders-repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Order, OrderSchema } from './schemas/orde-schema';
 import { GetOrdersService } from './services/get-orders.service';
+import { BILLING_SERVICE } from './constants/services';
 
 @Module({
   imports: [
@@ -26,6 +27,9 @@ import { GetOrdersService } from './services/get-orders.service';
         schema: OrderSchema,
       },
     ]),
+    RmqModule.register({
+      name: BILLING_SERVICE,
+    }),
   ],
   controllers: [OrdersController],
   providers: [CreateOrdersService, GetOrdersService, OrdersRepository],
