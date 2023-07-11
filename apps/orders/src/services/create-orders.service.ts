fd8789564266791dab3b60c199733c5e64f25406
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-07-11 13:53:28 -0300

    refactor: adjust create order with try catch

--- apps/orders/src/services/create-orders.service.ts
+++ apps/orders/src/services/create-orders.service.ts
@@ -1,11 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateOrderDTO } from '../dtos/create-order';
 import { OrdersRepository } from '../repositories/orders-repository';
+import { BILLING_SERVICE } from '../constants/services';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class CreateOrdersService {
-  constructor(private readonly ordersRepository: OrdersRepository) {}
+  constructor(
+    private readonly ordersRepository: OrdersRepository,
+    @Inject(BILLING_SERVICE) private billingClient: ClientProxy,
+  ) {}
   async execute(data: CreateOrderDTO) {
-    return await this.ordersRepository.create(data);
+    const session = await this.ordersRepository.startTransaction();
+    try {
+      const order = await this.ordersRepository.create(data, { session });
+
+      await lastValueFrom(
+        this.billingClient.emit('order_created', {
+          data,
+        }),
+      );
+
+      await session.commitTransaction();
+
+      return order;
+    } catch (err) {
+      await session.abortTransaction();
+
+      throw err;
+    }
   }
 }
